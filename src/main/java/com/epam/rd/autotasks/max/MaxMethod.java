02c7d1f4d03cc46c8177867d2dba184ebb260b5d
Dragaar <rosivanyshyn@gmail.com>
2022-09-04 14:09:19 +0300

    111

--- src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
+++ src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
@@ -1,9 +1,17 @@
 package com.epam.rd.autotasks.max;
 
+import java.util.Optional;
 import java.util.OptionalInt;
 
 public class MaxMethod {
     public static OptionalInt max(int[] values) {
-        throw new UnsupportedOperationException();
+
+        if(values != null && values.length > 0) {
+            int temp = values[0];
+
+            for (int e : values) if (e > temp) temp = e;
+            return OptionalInt.of(temp);
+
+        } else return OptionalInt.empty();
     }
 }
